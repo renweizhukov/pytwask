f35bb9f5ab9c0c80f320273951d6c9171c28f83e
Wei Ren <renwei2004@gmail.com>
2018-04-08 02:48:11 -0700

    Fix the encrypted PyPI password in .travis.yml again.
    Also fix the mismatched version in the Sphinx documentation.
    
    Change-Id: If07c6212724936bcd03e6610ee2a3332589e59d3
    Signed-off-by: Wei Ren <renwei2004@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ author = 'Wei Ren'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
